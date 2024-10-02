094b41b17f3d2f173a1f81098b01323eff05d587
user <kelsonlean@gmail.com>
2024-10-01 22:33:43 -0300

    tratamento e concatenacao

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,14 +1,29 @@
 import './calculadora.css';
 import { Container, Row, Col, Button, Form } from 'react-bootstrap';
 import {useState} from 'react';
+import CalculadoraService from './calculadora.service';
 
 
 function Calculadora() {
 
+  const [calcular, concatenarNumero, SOMA, SUBTRACAO, DIVISAO, MULTIPLICACAO] = CalculadoraService();
+
   const [txtNumeros, setTxtNumeros] = useState('0');
+  const [numero1, setNumero1] = useState('0');
+  const [numero2, setNumero2] = useState(null);
+  const[operacao, setOperacao] = useState(null);
 
   function adicionarNumero(numero) {
-    setTxtNumeros(txtNumeros + numero);
+    let resultado;
+    if(operacao ===null) {
+      resultado = concatenarNumero(numero1, numero);
+      setNumero1(resultado);
+    } else {
+      resultado = concatenarNumero(numero2, numero);
+      setNumero2(resultado);
+    }
+
+    setTxtNumeros(resultado);
   }
 
   function definirOperacao(op) {
@@ -101,7 +116,8 @@ function Calculadora() {
             onClick={() => adicionarNumero('0')}>0</Button>
           </Col>
           <Col>
-            <Button variant="light">.</Button>
+            <Button variant="light"
+            onClick={() => adicionarNumero('.')}>.</Button>
           </Col>
           <Col>
             <Button variant="success">=</Button>
